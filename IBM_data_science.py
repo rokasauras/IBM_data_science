64d4ced9fd94bdc0d46f90cd183ab92d8780cec3
rokasauras <125209408+rokasauras@users.noreply.github.com>
2024-09-20 16:45:40 +0100

    Update IBM_data_science.py

--- IBM_data_science.py
+++ IBM_data_science.py
@@ -1,2 +1,18 @@
 import pandas as pd  # import pandas library
 import numpy as np  # import numpy library
+import re  # import regular expression library
+
+# Read the CSV file directly using pandas
+recipes = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0103EN-SkillsNetwork/labs/Module%202/recipes.csv")
+
+# Display all columns
+pd.set_option('display.max_columns', None)
+
+# Show that data is read
+print("Data read into dataframe!")
+
+ingredients = list(recipes.columns.values)
+
+print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(rice).*")).search(ingredient)] if match])
+print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(wasabi).*")).search(ingredient)] if match])
+print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(soy).*")).search(ingredient)] if match])
