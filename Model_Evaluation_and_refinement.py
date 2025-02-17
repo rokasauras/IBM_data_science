356679339e2fc77437e4279a9378db0f7d13bfd0
rokasauras <rokas.jelinskas@gmail.com>
2025-02-17 12:18:26 +0000

    Add R² score evaluation for polynomial regression across multiple degrees

--- Model_Evaluation_and_refinement.py
+++ Model_Evaluation_and_refinement.py
@@ -143,3 +143,27 @@ print("Predicted values:", yhat[0:4])
 print("True values:", y_test[0:4].values)
 
 PollyPlot(x_train['horsepower'], x_test['horsepower'], y_train, y_test, poly,pr)
+
+poly.score(x_train_pr, y_train)
+
+poly.score(x_test_pr, y_test)
+
+Rsqu_test = []
+
+order = [1, 2, 3, 4]
+for n in order:
+    pr = PolynomialFeatures(degree=n)
+    
+    x_train_pr = pr.fit_transform(x_train[['horsepower']])
+    
+    x_test_pr = pr.fit_transform(x_test[['horsepower']])    
+    
+    lr.fit(x_train_pr, y_train)
+    
+    Rsqu_test.append(lr.score(x_test_pr, y_test))
+
+plt.plot(order, Rsqu_test)
+plt.xlabel('order')
+plt.ylabel('R^2')
+plt.title('R^2 Using Test Data')
+plt.text(3, 0.75, 'Maximum R^2 ')    
